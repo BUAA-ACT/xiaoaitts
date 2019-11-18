91ce82c044690bc57b657ce13fe26eda98a161e9
barrierye <barriery@qq.com>
2019-11-18 15:20:14 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'xiaoaitts',
-    packages = find_packages(where='.'),
-    version = '0.0.0',
-    description = 'xiaoai TTS API',
+    name='xiaoaitts',
+    packages=find_packages(where='.'),
+    version='0.0.0',
+    description='the TTS API for XiaoAi Wifispeaker.',
+    keywords='xiaoai wifispeaker TTS API',
     author='barriery',
     url='https://github.com/barrierye/xiaoaitts',
     install_requires=[
